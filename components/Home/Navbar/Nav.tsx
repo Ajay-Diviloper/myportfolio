5f738417c0b73db686872659e50862f28010f152
ashi6969 <ajay.spp.333@gmail.com>
2025-03-22 11:58:33 +0530

    header issue resolve

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -29,14 +29,14 @@ return ()=> {
   }, [])
   return (
     <nav className="fixed top-0 left-0 w-full h-[12vh] bg-blue-950 z-10 flex items-center justify-center px-4">
-      <div className="flex items-center h-full justify-between w-[95%] sm:w-[90%] xl:w-[80%] mx-auto">
+      <div className="flex items-center h-full justify-between w-[100%] sm:w-[90%] xl:w-[80%] mx-auto">
         {/* Logo */}
     
             <Image
           src="/images/logo.png"
           alt="Company Logo"
-          width={170}
-          height={170}
+          width={150}
+          height={150}
           priority
           className="text-left"
         />
@@ -54,7 +54,7 @@ return ()=> {
 
           {/* CTA & Mobile Menu Icon */}
           <div className="flex items-center space-x-4">
-            <button className="cursor-pointer hover:bg-gray-200 md:px-10 md:py-3 px-8 py-3 text-blue-800 font-semibold sm:text-base text-sm bg-white rounded-lg">
+            <button className="cursor-pointer hover:bg-gray-200 md:px-10 md:py-3 px-4 py-2 text-blue-800 font-semibold sm:text-base text-sm bg-white rounded-lg">
               Click Me
             </button>
 
